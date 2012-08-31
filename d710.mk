2f507b296d0d97d61b274dba82f1e441f7dc8387
PhantomPhreak53 <dapimp53@yahoo.com>
2012-08-31 08:49:19 -0700

    Cleanup

--- d710.mk
+++ d710.mk
@@ -17,36 +17,36 @@ DEVICE_PACKAGE_OVERLAYS := device/samsung/d710/overlay \
 		
 # Init files
 PRODUCT_COPY_FILES += \
-	device/samsung/galaxys2-common/lpm.rc:root/lpm.rc \
-	device/samsung/d710/init.smdk4210.rc:root/init.smdk4210.rc \
-	device/samsung/galaxys2-common/init.smdk4210.usb.rc:root/init.smdk4210.usb.rc \
-	device/samsung/d710/ueventd.smdk4210.rc:root/ueventd.smdk4210.rc
+    device/samsung/galaxys2-common/lpm.rc:root/lpm.rc \
+    device/samsung/d710/init.smdk4210.rc:root/init.smdk4210.rc \
+    device/samsung/galaxys2-common/init.smdk4210.usb.rc:root/init.smdk4210.usb.rc \
+    device/samsung/d710/ueventd.smdk4210.rc:root/ueventd.smdk4210.rc
 
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
-	frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
-	frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
-	frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
-	frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
-	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-	frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
-	frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
-	frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
-	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-	frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-	frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
-	frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-	frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
-	frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml 
+    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \
+    frameworks/native/data/etc/android.hardware.camera.autofocus.xml:system/etc/permissions/android.hardware.camera.autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.xml:system/etc/permissions/android.hardware.camera.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.location.xml:system/etc/permissions/android.hardware.location.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:system/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.xml:system/etc/permissions/android.hardware.touchscreen.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.software.sip.xml:system/etc/permissions/android.software.sip.xml \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml 
 	
 # Keylayout
 PRODUCT_COPY_FILES += \
@@ -72,38 +72,38 @@ PRODUCT_COPY_FILES += \
 	
 # Keychars
 PRODUCT_COPY_FILES += \
-   device/samsung/d710/usr/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
-   device/samsung/d710/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
-   device/samsung/d710/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
-   device/samsung/d710/usr/keychars/Virtual.kcm:system/usr/keychars/Virtual.kcm
+    device/samsung/d710/usr/keychars/Generic.kcm:system/usr/keychars/Generic.kcm \
+    device/samsung/d710/usr/keychars/qwerty.kcm:system/usr/keychars/qwerty.kcm \
+    device/samsung/d710/usr/keychars/qwerty2.kcm:system/usr/keychars/qwerty2.kcm \
+    device/samsung/d710/usr/keychars/Virtual.kcm:system/usr/keychars/Virtual.kcm
 
 #idc
 PRODUCT_COPY_FILES += \
-   device/samsung/d710/usr/idc/melfas_ts.idc:system/usr/idc/melfas_ts.idc \
-   device/samsung/d710/usr/idc/mxt224_ts_input.idc:system/usr/idc/mxt224_ts_input.idc \
-   device/samsung/d710/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
-   device/samsung/d710/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
-   device/samsung/d710/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc 
+    device/samsung/d710/usr/idc/melfas_ts.idc:system/usr/idc/melfas_ts.idc \
+    device/samsung/d710/usr/idc/mxt224_ts_input.idc:system/usr/idc/mxt224_ts_input.idc \
+    device/samsung/d710/usr/idc/qwerty.idc:system/usr/idc/qwerty.idc \
+    device/samsung/d710/usr/idc/qwerty2.idc:system/usr/idc/qwerty2.idc \
+    device/samsung/d710/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc 
 
 # Audio
 PRODUCT_COPY_FILES += \
-	device/samsung/galaxys2-common/configs/asound.conf:system/etc/asound.conf
+    device/samsung/galaxys2-common/configs/asound.conf:system/etc/asound.conf
     	
 # Misc	
 PRODUCT_COPY_FILES += \
-	device/samsung/d710/configs/apns-conf.xml:system/etc/apns-conf.xml
+    device/samsung/d710/configs/apns-conf.xml:system/etc/apns-conf.xml
 	
 # gps
 PRODUCT_COPY_FILES += \
-  device/samsung/d710/gpsfix:system/bin/gpsfix
+    device/samsung/d710/gpsfix:system/bin/gpsfix
 	
 # Bluetooth configuration files
 PRODUCT_COPY_FILES += \
-	system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
+    system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
 	
 # Vold
 PRODUCT_COPY_FILES += \
-	device/samsung/d710/configs/vold.fstab:system/etc/vold.fstab 
+    device/samsung/d710/configs/vold.fstab:system/etc/vold.fstab 
 
 # The OpenGL ES API level that is natively supported by this device.
 # This is a 16.16 fixed point number
@@ -114,19 +114,19 @@ PRODUCT_PROPERTY_OVERRIDES := \
 # Note that the only such settings should be the ones that are too low-level to
 # be reachable from resources or other mechanisms.
 PRODUCT_PROPERTY_OVERRIDES += \
-       wifi.interface=wlan0 \
-       wifi.supplicant_scan_interval=240 \
-       ro.telephony.sends_barcount=1 \
-       ro.ril.def.agps.mode=2 \
-       ro.telephony.call_ring.multiple=false \
-       ro.telephony.call_ring.delay=3000 \
-	   hwui.render_dirty_regions=false
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=240 \
+    ro.telephony.sends_barcount=1 \
+    ro.ril.def.agps.mode=2 \
+    ro.telephony.call_ring.multiple=false \
+    ro.telephony.call_ring.delay=3000 \
+    hwui.render_dirty_regions=false
 
 PRODUCT_PROPERTY_OVERRIDES += \
-       persist.sys.vold.switchexternal=1 \
-	   persist.service.usb.setting=0 \
-	   persist.service.adb.enable=1 \
-	   persist.sys.usb.config=mass_storage,adb
+    persist.sys.vold.switchexternal=1 \
+    persist.service.usb.setting=0 \
+    persist.service.adb.enable=1 \
+    persist.sys.usb.config=mass_storage,adb
 	   
 # Telephony property for CDMA
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -148,26 +148,26 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	
 # Gps
 PRODUCT_COPY_FILES += \
-	device/samsung/d710/configs/gps.conf:system/etc/gps.conf \
-	device/samsung/d710/configs/sirfgps.conf:system/etc/sirfgps.conf
+    device/samsung/d710/configs/gps.conf:system/etc/gps.conf \
+    device/samsung/d710/configs/sirfgps.conf:system/etc/sirfgps.conf
 
 # Packages
 PRODUCT_PACKAGES := \
     TvOut \
-	audio.a2dp.default \
-	com.android.future.usb.accessory \
-	smdk4210_hdcp_keys \
-	GalaxyS2Settings \
-	SamsungServiceMode \
-	libsurfaceflinger_client
+    audio.a2dp.default \
+    com.android.future.usb.accessory \
+    smdk4210_hdcp_keys \
+    GalaxyS2Settings \
+    SamsungServiceMode \
+    libsurfaceflinger_client
 	
 # Camera
 PRODUCT_PACKAGES += \
-	camera.exynos4
+    camera.exynos4
 	
 PRODUCT_COPY_FILES += \
-	device/samsung/galaxys2-common/configs/media_profiles.xml:system/etc/media_profiles.xml \
-	device/samsung/galaxys2-common/configs/media_codecs.xml:system/etc/media_codecs.xml
+    device/samsung/galaxys2-common/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    device/samsung/galaxys2-common/configs/media_codecs.xml:system/etc/media_codecs.xml
 	
 # Charger
 PRODUCT_PACKAGES += \
@@ -176,14 +176,14 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-	lights.exynos4 \
-	sensors.exynos4
+    lights.exynos4 \
+    sensors.exynos4
 
 # Filesystem management tools
 PRODUCT_PACKAGES += \
-	static_busybox \
-	make_ext4fs \
-	setup_fs
+    static_busybox \
+    make_ext4fs \
+    setup_fs
 	
 # WIMAX
 PRODUCT_PACKAGES += \
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.wimax.interface=uwbr0 \
-	persist.service.usb.hubport=4
+    persist.service.usb.hubport=4
 	
 # MFC API
 PRODUCT_PACKAGES += \
@@ -205,7 +205,7 @@ TARGET_HAL_PATH := hardware/samsung/exynos4/hal
 TARGET_OMX_PATH := hardware/samsung/exynos/multimedia/openmax
 
 PRODUCT_COPY_FILES += \
-	device/samsung/d710/configs/secomxregistry:system/etc/secomxregistry
+    device/samsung/d710/configs/secomxregistry:system/etc/secomxregistry
 
 PRODUCT_PACKAGES += \
     libstagefrighthw \
@@ -224,15 +224,15 @@ PRODUCT_PACKAGES += \
     libOMX.SEC.M4V.Encoder \
     libSEC_OMX_Adec \
     libOMX.SEC.MP3.Decoder \
-	libhwconverter \
-	libs5pjpeg \
-	libfimg
+    libhwconverter \
+    libs5pjpeg \
+    libfimg
 
 # enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
 PRODUCT_PROPERTY_OVERRIDES += \
-        ro.com.google.locationfeatures=1 \
-        ro.com.google.networklocation=1
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1
 
 # Extended JNI checks
 # The extended JNI checks will cause the system to run more slowly, but they can spot a variety of nasty bugs 
